f02489c7de095e593255ed190ffa8772d33044d7
cyim12 <71360341+cyim12@users.noreply.github.com>
2021-03-12 23:27:34 -0500

    js

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ const oscarObj = document.getElementById('oscar');
 const pamObj = document.getElementById('pam');
 const phyllisObj = document.getElementById('phyllis');
 const stanleyObj = document.getElementById('stanley');
-
+ 
 darrylObj.addEventListener('click', function(evt) {
     console.log('darryl button clicked' + evt.target.id);
     const idName = evt.target.id;
@@ -83,7 +83,48 @@ stanleyObj.addEventListener('click', function(evt) {
     userPicked(idName);
 });
 
-//CALENDAR//
+function fade2Content() {
+  console.log('fade2Content called');
+  document.body.classList.remove("blackScreen");
+}
+
+const pisObj = document.getElementById('pis');
+const scoObj = document.getElementById('sco');
+const aquObj = document.getElementById('aqu');
+
+const displayObj = document.getElementById('display');
+
+pisObj.addEventListener('click', function (evt) {
+  console.log('pis button clicked: ' + evt.target.id);
+  const idName = evt.target.id;
+  userPicked(idName);
+});
+
+scoObj.addEventListener('click', function (evt) {
+  console.log('sco button clicked: ' + evt.target.id); 
+  const idName = evt.target.id;
+  userPicked(idName);
+});
+
+aquObj.addEventListener('click', function (evt) {
+  console.log('aqu button clicked: ' + evt.target.id);
+  const idName = evt.target.id;
+  userPicked(idName);
+});
+
+MODALS
+const modal = document.getElementById('modal');
+ 
+const closeModalObj = document.getElementById('btn-modal-close');
+closeModalObj.addEventListener('click', function(){
+  modal.hidden = !modal.hidden;
+});
+ 
+const zDisplayObj = document.getElementById('zDisplayImg');
+const zTitleObj = document.getElementById('zTitle');
+const zDateRangeObj = document.getElementById('zDateRange');
+const zMoreINfoObj = document.getElementById('zMoreInfo');
+  
 const userSubmitObj = document.getElementById('userSubmit');
  
 userSubmitObj.addEventListener('click', function(){
@@ -130,109 +171,3 @@ userSubmitObj.addEventListener('click', function(){
       console.log('AstroSign is: ' + AstroSign);
       userPicked(AstroSign);
 });
-
-
-//MODALS//
-function userPicked(whichOne) {
-  console.log('userPicked called: ' + whichOne);
-  switch (whichOne) {
-    case 'darryl':
-      zDisplayObj.src = '/images/darrylphilbin.png';
-      zTitleObj.innerHTML = 'Darryl Philbin';
-      zDateRangeObj.innerHTML = '20-January to 18-February';
-      zMoreInfo.innerHTML = '<p>pls put filler here</p>';
-      modal.hidden = !modal.hidden;
-      break;
-    case 'angela':
-      zDisplayObj.src = '/images/angelamartin.png';
-      zTitleObj.innerHTML = 'Angela Martin';
-      zDateRangeObj.innerHTML = '19-February to 20-March';
-      zMoreInfo.innerHTML = '<p>pls put filler here</p>';
-      modal.hidden = !modal.hidden;
-      break;
-    case 'andy':
-      zDisplayObj.src = '/images/andybernard.png';
-      zTitleObj.innerHTML = 'Andy Bernard';
-      zDateRangeObj.innerHTML = '21-March to 19-April';
-      zMoreInfo.innerHTML = '<p>pls put filler here</p>';
-      modal.hidden = !modal.hidden;
-      break;
-    case 'kevin':
-      zDisplayObj.src = '/images/kevinmalone.png';
-      zTitleObj.innerHTML = 'Kevin Malone';
-      zDateRangeObj.innerHTML = '20-April to 20-May';
-      zMoreInfo.innerHTML = '<p>pls put filler here</p>';
-      modal.hidden = !modal.hidden;
-      break;
-    case 'dwight':
-      zDisplayObj.src = '/images/dwightshrute.png';
-      zTitleObj.innerHTML = 'Dwight Shrute';
-      zDateRangeObj.innerHTML = '21-May to 21-June';
-      zMoreInfo.innerHTML = '<p>pls put filler here</p>';
-      modal.hidden = !modal.hidden;
-        break;
-      case 'jim':
-        zDisplayObj.src = '/images/jimhalpert.png';
-        zTitleObj.innerHTML = 'Jim Halpert';
-        zDateRangeObj.innerHTML = '22-June to 22-July';
-        zMoreInfo.innerHTML = '<p>pls put filler here</p>';
-        modal.hidden = !modal.hidden;
-        break;
-      case 'kelly':
-        zDisplayObj.src = '/images/kellykapoor.png';
-        zTitleObj.innerHTML = 'Kelly Kapoor';
-        zDateRangeObj.innerHTML = '23-July to 22-August';
-        zMoreInfo.innerHTML = '<p>pls put filler here</p>';
-        modal.hidden = !modal.hidden;
-        break;
-      case 'michael':
-        zDisplayObj.src = '/images/michaelscott.png';
-        zTitleObj.innerHTML = 'Michael Scott';
-        zDateRangeObj.innerHTML = '23-August to 22-September';
-        zMoreInfo.innerHTML = '<p>pls put filler here</p>';
-        modal.hidden = !modal.hidden;
-        break;
-      case 'oscar':
-        zDisplayObj.src = '/images/oscarnunez.png';
-        zTitleObj.innerHTML = 'Oscar Nunez';
-        zDateRangeObj.innerHTML = '23-September to 23-October';
-        zMoreInfo.innerHTML = '<p>pls put filler here</p>';
-        modal.hidden = !modal.hidden;
-        break;
-      case 'pam':
-        zDisplayObj.src = '/images/pambeesly.png';
-        zTitleObj.innerHTML = 'Pam Beesly';
-        zDateRangeObj.innerHTML = '24-October to 21-November';
-        zMoreInfo.innerHTML = '<p>pls put filler here</p>';
-        modal.hidden = !modal.hidden;
-        break;
-      case 'phyllis':
-        zDisplayObj.src = '/images/phyllissmith.png';
-        zTitleObj.innerHTML = 'Phyllis Smith';
-        zDateRangeObj.innerHTML = '22-November to 21-December';
-        zMoreInfo.innerHTML = '<p>pls put filler here</p>';
-        modal.hidden = !modal.hidden;
-        break;
-      case 'stanley':
-        zDisplayObj.src = '/images/stanleyhudson.png';
-        zTitleObj.innerHTML = 'Stanley Hudson';
-        zDateRangeObj.innerHTML = '22-December to 19-January';
-        zMoreInfo.innerHTML = '<p>pls put filler here</p>';
-        modal.hidden = !modal.hidden;
-        break;
-    default: 
-    }
-  }
-
-//MODALS//
-const modal = document.getElementById('modal');
- 
-const closeModalObj = document.getElementById('btn-modal-close');
-closeModalObj.addEventListener('click', function(){
-  modal.hidden = !modal.hidden;
-});
- 
-const zDisplayObj = document.getElementById('zDisplayImg');
-const zTitleObj = document.getElementById('zTitle');
-const zDateRangeObj = document.getElementById('zDateRange');
-const zMoreINfoObj = document.getElementById('zMoreInfo');
