44ccbe11303b584056599ac7213b042836c35548
cyim12 <71360341+cyim12@users.noreply.github.com>
2021-03-13 18:59:46 -0500

    rename

--- main.js
+++ main.js
@@ -102,29 +102,29 @@ userSubmitObj.addEventListener('click', function(){
     let AstroSign = "";
     
     if ((whichMonth == 12 && whichDayOfMonth >= 22) || (whichMonth == 1 && whichDayOfMonth <= 19)) {
-        AstroSign = "drums";
+        AstroSign = "stanley";
       } else if ((whichMonth == 11 && whichDayOfMonth >= 22) || (whichMonth == 12 && whichDayOfMonth <= 21)) {
-        AstroSign = "pipes";
+        AstroSign = "phyllis";
       } else if ((whichMonth == 10 && whichDayOfMonth >= 24) || (whichMonth == 11 && whichDayOfMonth <= 21)) {
-        AstroSign = "lord";
+        AstroSign = "pam";
       } else if ((whichMonth == 9 && whichDayOfMonth >= 23) || (whichMonth == 10 && whichDayOfMonth <= 23)) {
-        AstroSign = "lady";
+        AstroSign = "oscar";
       } else if ((whichMonth == 8 && whichDayOfMonth >= 23) || (whichMonth == 9 && whichDayOfMonth <= 22)) {
-        AstroSign = "maid";
+        AstroSign = "michael";
       } else if ((whichMonth == 7 && whichDayOfMonth >= 23) || (whichMonth == 8 && whichDayOfMonth <= 22)) {
-        AstroSign = "swan";
+        AstroSign = "kelly";
       } else if ((whichMonth == 6 && whichDayOfMonth >= 22) || (whichMonth == 7 && whichDayOfMonth <= 22)) {
-        AstroSign = "geese";
+        AstroSign = "jim";
       } else if ((whichMonth == 5 && whichDayOfMonth >= 21) || (whichMonth == 6 && whichDayOfMonth <= 21)) {
-        AstroSign = "ring";
+        AstroSign = "dwight";
       } else if ((whichMonth == 4 && whichDayOfMonth >= 20) || (whichMonth == 5 && whichDayOfMonth <= 20)) {
-        AstroSign = "bird";
+        AstroSign = "kevin";
       } else if ((whichMonth == 3 && whichDayOfMonth >= 21) || (whichMonth == 4 && whichDayOfMonth <= 19)) {
-        AstroSign = "hen";
+        AstroSign = "andy";
       } else if ((whichMonth == 2 && whichDayOfMonth >= 19) || (whichMonth == 3 && whichDayOfMonth <= 20)) {
-        AstroSign = "turtle";
+        AstroSign = "angela";
       } else if ((whichMonth == 1 && whichDayOfMonth >= 20) || (whichMonth == 2 && whichDayOfMonth <= 18)) {
-        AstroSign = "tree";
+        AstroSign = "darryl";
       }
  
       console.log('AstroSign is: ' + AstroSign);
