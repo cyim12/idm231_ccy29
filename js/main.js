584fef160a873b2cab6bc833af0863a2036a9264
cyim12 <71360341+cyim12@users.noreply.github.com>
2021-03-17 15:49:37 -0400

    descriptions

--- js/main.js
+++ js/main.js
@@ -148,7 +148,7 @@ function userPicked(whichOne) {
       zDisplayObj.src = '/gif/stanley.gif';
       zTitleObj.innerHTML = 'Stanley Hudson';
       zDateRangeObj.innerHTML = 'Apr. 20 ~ May. 20';
-      zMoreInfoObj.innerHTML = '<p> Stanley is generally quiet and polite, but don’t cross him. When Michael gets too loose-jawed, Stanley can put him in his place with a single intimidating glance. But overall, Stanley mostly wants to be left alone to enjoy his simple pleasures of puzzles and pretzels.</p>';
+      zMoreInfoObj.innerHTML = '<p>Stanley is generally quiet and polite, but don’t cross him. When Michael gets too loose-jawed, Stanley can put him in his place with a single intimidating glance. But overall, Stanley mostly wants to be left alone to enjoy his simple pleasures of puzzles and pretzels.</p>';
       modal.hidden = !modal.hidden;
       break;
 
@@ -156,7 +156,7 @@ function userPicked(whichOne) {
       zDisplayObj.src = '/gif/oscar.gif';
       zTitleObj.innerHTML = 'Oscar Martinez';
       zDateRangeObj.innerHTML = 'May. 21 ~ Jun. 21';
-      zMoreInfoObj.innerHTML = '<p>pls put filler here</p>';
+      zMoreInfoObj.innerHTML = '<p>Oscar is pleasant but distant, and a bit of a know-it-all. In meetings, Oscar is inquisitive and critical. In conversation, he’s usually either friendly or pedantic. Like his co-worker Stanley, he’s generally uninterested in close relationships with his co-workers.</p>';
       modal.hidden = !modal.hidden;
       break;  
 
@@ -164,7 +164,7 @@ function userPicked(whichOne) {
       zDisplayObj.src = '/gif/pam.gif';
       zTitleObj.innerHTML = 'Pam Beesly';
       zDateRangeObj.innerHTML = 'Jun. 22 ~ Jul. 22';
-      zMoreInfoObj.innerHTML = '<p>pls put filler here</p>';
+      zMoreInfoObj.innerHTML = '<p>Pam is friendly and sweet, but shy and a bit mousy. Pam’s never been the type to take a stand or “go for it.” She says more with her face than with words. She tolerates without complaint. These aren’t qualities she admires about herself. She’s actively trying to change them.</p>';
       modal.hidden = !modal.hidden;
       break;
 
@@ -172,7 +172,7 @@ function userPicked(whichOne) {
       zDisplayObj.src = '/gif/andy.gif';
       zTitleObj.innerHTML = 'Andy Bernard';
       zDateRangeObj.innerHTML = 'Jul. 23 ~ Aug. 22';
-      zMoreInfoObj.innerHTML = '<p>pls put filler here</p>';
+      zMoreInfoObj.innerHTML = '<p>Andy is clueless and often obnoxious, but never malicious. Andy gives people unwanted nicknames, goes on about his Ivy League education, and is completely tone-deaf on issues of class and race – and yet he’s a nice guy at heart. There’s a genuine goodness in him. Even his obliviousness is genuine. </p>';
       modal.hidden = !modal.hidden;
       break;
 
@@ -180,7 +180,7 @@ function userPicked(whichOne) {
       zDisplayObj.src = '/gif/jim.gif';
       zTitleObj.innerHTML = 'Jim Halpert';
       zDateRangeObj.innerHTML = 'Aug. 23 ~ Sept. 22';
-      zMoreInfoObj.innerHTML = '<p>pls put filler here</p>';
+      zMoreInfoObj.innerHTML = '<p>Jim is charismatic and charming, and not in a creepy way. Jim is a great guy: funny, kind, mild-mannered. Jim is also dedicated as he is part of a rivalrous pranking on fellow salesman and invested in a romantic interest with the receptionist.</p>';
       modal.hidden = !modal.hidden;
       break;
     
@@ -188,15 +188,15 @@ function userPicked(whichOne) {
       zDisplayObj.src = '/gif/phyllis.gif';
       zTitleObj.innerHTML = 'Phyllis Smith';
       zDateRangeObj.innerHTML = 'Sept. 23 ~ Oct. 23';
-      zMoreInfoObj.innerHTML = '<p>pls put filler here</p>';
+      zMoreInfoObj.innerHTML = '<p>Phyllis is quiet and nuturing. She rarely starts conflict but isn’t above blackmail – in fact, that’s how she took control of the Party Planning Committee. Phyllis sometimes lacks self-esteem, yet beams about her happy marriage. Beneath her quiet exterior lurks a strong, determined woman.</p>';
       modal.hidden = !modal.hidden;
       break;
 
     case 'dwight':
       zDisplayObj.src = '/gif/dwight.gif';
-      zTitleObj.innerHTML = 'Dwight Shrute';
+      zTitleObj.innerHTML = 'Dwight Schrute';
       zDateRangeObj.innerHTML = 'Oct. 24 ~ Nov. 21';
-      zMoreInfoObj.innerHTML = '<p>pls put filler here</p>';
+      zMoreInfoObj.innerHTML = '<p>Dwight can easily be described as the most ambitious Dunder Mifflin employee as his goal in life was to become successful. Under the hard exterior, there’s a soft side to Dwight — something else that makes him appealing. But as lovable as Dwight can be, many things about him make no sense. </p>';
       modal.hidden = !modal.hidden;
       break;
 
@@ -204,7 +204,7 @@ function userPicked(whichOne) {
       zDisplayObj.src = '/gif/kelly.gif';
       zTitleObj.innerHTML = 'Kelly Kapoor';
       zDateRangeObj.innerHTML = 'Nov. 22 ~ Dec. 21';
-      zMoreInfoObj.innerHTML = '<p>pls put filler here</p>';
+      zMoreInfoObj.innerHTML = '<p>Kelly is always happy to provide needed distractions to anyone who’ll listen. We love Kelly because she loves who she is, and her lack of desire to be anything other than that should serve as an example for all to embrace the trivial, give in to the pleasures of gossip, and consider starting some of their own.</p>';
       modal.hidden = !modal.hidden;
       break;
 
@@ -212,7 +212,7 @@ function userPicked(whichOne) {
       zDisplayObj.src = '/gif/darryl.gif';
       zTitleObj.innerHTML = 'Darryl Philbin';
       zDateRangeObj.innerHTML = 'Dec. 22 ~ Jan. 19';
-      zMoreInfoObj.innerHTML = '<p>pls put filler here</p>';
+      zMoreInfoObj.innerHTML = '<p>Darryl is mellow and well-liked. Darryl is competent, ambitious, innovative, chill, funny and loves to take advantage of his boss. His rapport with the rest of the staff both in the office and warehouse is friendly. When given an opportunity, Darryl takes it and invests in it for the long run.</p>';
       modal.hidden = !modal.hidden;
       break;
 
@@ -220,7 +220,7 @@ function userPicked(whichOne) {
       zDisplayObj.src = '/gif/michael.gif';
       zTitleObj.innerHTML = 'Michael Scott';
       zDateRangeObj.innerHTML = 'Jan. 20 ~ Feb. 18';
-      zMoreInfoObj.innerHTML = '<p>pls put filler here</p>';
+      zMoreInfoObj.innerHTML = '<p>Michael is vain and oblivious, but well-meaning. He is often inappropriate and completely unaware that he’s rubbing people the wrong way. Yet just when you’re ready to write him off forever as a clueless buffoon, he does something nice for a friend to show that his heart’s ultimately in the right place.  </p>';
       modal.hidden = !modal.hidden;
       break;
 
@@ -228,7 +228,7 @@ function userPicked(whichOne) {
       zDisplayObj.src = '/gif/kevin.gif';
       zTitleObj.innerHTML = 'Kevin Malone';
       zDateRangeObj.innerHTML = 'Feb. 19 ~ Mar. 20';
-      zMoreInfoObj.innerHTML = '<p>pls put filler here</p>';
+      zMoreInfoObj.innerHTML = '<p>Kevin is lovably dumb even though he thinks he is a genius. He’s somehow openly perverse yet completely harmless, even sweet. Kevin is charmingly oblivious, even when his cluelessness leads to backhanded compliments to his female co-workers. Nobody seems to mind that much; that’s just Kevin.</p>';
       modal.hidden = !modal.hidden;
       break;
     default: 
