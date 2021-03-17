c49bec2d5fe93fd2e9f1f58777cd71e8ef11863d
cyim12 <71360341+cyim12@users.noreply.github.com>
2021-03-17 11:43:25 -0400

    js

--- js/main.js
+++ js/main.js
@@ -11,7 +11,6 @@ const darrylObj = document.getElementById('darryl');
 const michaelObj = document.getElementById('michael');
 const kevinObj = document.getElementById('kevin');
 
- 
 angelaObj.addEventListener('click', function(evt) {
     console.log('angela button clicked' + evt.target.id);
     const idName = evt.target.id;
@@ -103,29 +102,29 @@ userSubmitObj.addEventListener('click', function(){
     let AstroSign = "";
     
     if ((whichMonth == 12 && whichDayOfMonth >= 22) || (whichMonth == 1 && whichDayOfMonth <= 19)) {
-        AstroSign = "kevin";
-      } else if ((whichMonth == 11 && whichDayOfMonth >= 22) || (whichMonth == 12 && whichDayOfMonth <= 21)) {
-        AstroSign = "michael";
-      } else if ((whichMonth == 10 && whichDayOfMonth >= 24) || (whichMonth == 11 && whichDayOfMonth <= 21)) {
         AstroSign = "darryl";
-      } else if ((whichMonth == 9 && whichDayOfMonth >= 23) || (whichMonth == 10 && whichDayOfMonth <= 23)) {
+      } else if ((whichMonth == 11 && whichDayOfMonth >= 22) || (whichMonth == 12 && whichDayOfMonth <= 21)) {
         AstroSign = "kelly";
-      } else if ((whichMonth == 8 && whichDayOfMonth >= 23) || (whichMonth == 9 && whichDayOfMonth <= 22)) {
+      } else if ((whichMonth == 10 && whichDayOfMonth >= 24) || (whichMonth == 11 && whichDayOfMonth <= 21)) {
         AstroSign = "dwight";
-      } else if ((whichMonth == 7 && whichDayOfMonth >= 23) || (whichMonth == 8 && whichDayOfMonth <= 22)) {
+      } else if ((whichMonth == 9 && whichDayOfMonth >= 23) || (whichMonth == 10 && whichDayOfMonth <= 23)) {
         AstroSign = "phyllis";
-      } else if ((whichMonth == 6 && whichDayOfMonth >= 22) || (whichMonth == 7 && whichDayOfMonth <= 22)) {
+      } else if ((whichMonth == 8 && whichDayOfMonth >= 23) || (whichMonth == 9 && whichDayOfMonth <= 22)) {
         AstroSign = "jim";
-      } else if ((whichMonth == 5 && whichDayOfMonth >= 21) || (whichMonth == 6 && whichDayOfMonth <= 21)) {
+      } else if ((whichMonth == 7 && whichDayOfMonth >= 23) || (whichMonth == 8 && whichDayOfMonth <= 22)) {
         AstroSign = "andy";
-      } else if ((whichMonth == 4 && whichDayOfMonth >= 20) || (whichMonth == 5 && whichDayOfMonth <= 20)) {
+      } else if ((whichMonth == 6 && whichDayOfMonth >= 22) || (whichMonth == 7 && whichDayOfMonth <= 22)) {
         AstroSign = "pam";
-      } else if ((whichMonth == 3 && whichDayOfMonth >= 21) || (whichMonth == 4 && whichDayOfMonth <= 19)) {
+      } else if ((whichMonth == 5 && whichDayOfMonth >= 21) || (whichMonth == 6 && whichDayOfMonth <= 21)) {
         AstroSign = "oscar";
-      } else if ((whichMonth == 2 && whichDayOfMonth >= 19) || (whichMonth == 3 && whichDayOfMonth <= 20)) {
+      } else if ((whichMonth == 4 && whichDayOfMonth >= 20) || (whichMonth == 5 && whichDayOfMonth <= 20)) {
         AstroSign = "stanley";
-      } else if ((whichMonth == 1 && whichDayOfMonth >= 20) || (whichMonth == 2 && whichDayOfMonth <= 18)) {
+      } else if ((whichMonth == 3 && whichDayOfMonth >= 21) || (whichMonth == 4 && whichDayOfMonth <= 19)) {
         AstroSign = "angela";
+      } else if ((whichMonth == 2 && whichDayOfMonth >= 19) || (whichMonth == 3 && whichDayOfMonth <= 20)) {
+        AstroSign = "kevin";
+      } else if ((whichMonth == 1 && whichDayOfMonth >= 20) || (whichMonth == 2 && whichDayOfMonth <= 18)) {
+        AstroSign = "michael";
       }
  
       console.log('AstroSign is: ' + AstroSign);
@@ -140,15 +139,15 @@ function userPicked(whichOne) {
     case 'angela':
       zDisplayObj.src = '/gif/angela.gif';
       zTitleObj.innerHTML = 'Angela Martin';
-      zDateRangeObj.innerHTML = 'Feb. 19 ~ Mar. 20';
+      zDateRangeObj.innerHTML = 'Mar. 21 ~ Apr. 19';
       zMoreInfo.innerHTML = '<p>pls put filler here</p>';
       modal.hidden = !modal.hidden;
       break;
-    
+
     case 'stanley':
       zDisplayObj.src = '/gif/stanley.gif';
       zTitleObj.innerHTML = 'Stanley Hudson';
-      zDateRangeObj.innerHTML = '22-December to 19-January';
+      zDateRangeObj.innerHTML = 'Apr. 20 ~ May. 20';
       zMoreInfo.innerHTML = '<p>pls put filler here</p>';
       modal.hidden = !modal.hidden;
       break;
@@ -156,15 +155,15 @@ function userPicked(whichOne) {
     case 'oscar':
       zDisplayObj.src = '/gif/oscar.gif';
       zTitleObj.innerHTML = 'Oscar Nunez';
-      zDateRangeObj.innerHTML = '23-September to 23-October';
+      zDateRangeObj.innerHTML = 'May. 21 ~ Jun. 21';
       zMoreInfo.innerHTML = '<p>pls put filler here</p>';
       modal.hidden = !modal.hidden;
-      break;
+      break;  
 
     case 'pam':
       zDisplayObj.src = '/gif/pam.gif';
       zTitleObj.innerHTML = 'Pam Beesly';
-      zDateRangeObj.innerHTML = '24-October to 21-November';
+      zDateRangeObj.innerHTML = 'Jun. 22 ~ Jul. 22';
       zMoreInfo.innerHTML = '<p>pls put filler here</p>';
       modal.hidden = !modal.hidden;
       break;
@@ -172,7 +171,7 @@ function userPicked(whichOne) {
     case 'andy':
       zDisplayObj.src = '/gif/andy.gif';
       zTitleObj.innerHTML = 'Andy Bernard';
-      zDateRangeObj.innerHTML = '21-March to 19-April';
+      zDateRangeObj.innerHTML = 'Jul. 23 ~ Aug. 22';
       zMoreInfo.innerHTML = '<p>pls put filler here</p>';
       modal.hidden = !modal.hidden;
       break;
@@ -180,15 +179,15 @@ function userPicked(whichOne) {
     case 'jim':
       zDisplayObj.src = '/gif/jim.gif';
       zTitleObj.innerHTML = 'Jim Halpert';
-      zDateRangeObj.innerHTML = '22-June to 22-July';
+      zDateRangeObj.innerHTML = 'Aug. 23 ~ Sept. 22';
       zMoreInfo.innerHTML = '<p>pls put filler here</p>';
       modal.hidden = !modal.hidden;
       break;
-
+    
     case 'phyllis':
       zDisplayObj.src = '/gif/phyllis.gif';
       zTitleObj.innerHTML = 'Phyllis Smith';
-      zDateRangeObj.innerHTML = '22-November to 21-December';
+      zDateRangeObj.innerHTML = 'Sept. 23 ~ Oct. 23';
       zMoreInfo.innerHTML = '<p>pls put filler here</p>';
       modal.hidden = !modal.hidden;
       break;
@@ -196,7 +195,7 @@ function userPicked(whichOne) {
     case 'dwight':
       zDisplayObj.src = '/gif/dwight.gif';
       zTitleObj.innerHTML = 'Dwight Shrute';
-      zDateRangeObj.innerHTML = '21-May to 21-June';
+      zDateRangeObj.innerHTML = 'Oct. 24 ~ Nov. 21';
       zMoreInfo.innerHTML = '<p>pls put filler here</p>';
       modal.hidden = !modal.hidden;
       break;
@@ -204,7 +203,7 @@ function userPicked(whichOne) {
     case 'kelly':
       zDisplayObj.src = '/gif/kelly.gif';
       zTitleObj.innerHTML = 'Kelly Kapoor';
-      zDateRangeObj.innerHTML = '23-July to 22-August';
+      zDateRangeObj.innerHTML = 'Nov. 22 ~ Dec. 21';
       zMoreInfo.innerHTML = '<p>pls put filler here</p>';
       modal.hidden = !modal.hidden;
       break;
@@ -212,7 +211,7 @@ function userPicked(whichOne) {
     case 'darryl':
       zDisplayObj.src = '/gif/darryl.gif';
       zTitleObj.innerHTML = 'Darryl Philbin';
-      zDateRangeObj.innerHTML = '20-January to 18-February';
+      zDateRangeObj.innerHTML = 'Dec. 22 ~ Jan. 19';
       zMoreInfo.innerHTML = '<p>pls put filler here</p>';
       modal.hidden = !modal.hidden;
       break;
@@ -220,7 +219,7 @@ function userPicked(whichOne) {
     case 'michael':
       zDisplayObj.src = '/gif/michael.gif';
       zTitleObj.innerHTML = 'Michael Scott';
-      zDateRangeObj.innerHTML = '23-August to 22-September';
+      zDateRangeObj.innerHTML = 'Jan. 20 ~ Feb. 18';
       zMoreInfo.innerHTML = '<p>pls put filler here</p>';
       modal.hidden = !modal.hidden;
       break;
@@ -228,7 +227,7 @@ function userPicked(whichOne) {
     case 'kevin':
       zDisplayObj.src = '/gif/kevin.gif';
       zTitleObj.innerHTML = 'Kevin Malone';
-      zDateRangeObj.innerHTML = '20-April to 20-May';
+      zDateRangeObj.innerHTML = 'Feb. 19 ~ Mar. 20';
       zMoreInfo.innerHTML = '<p>pls put filler here</p>';
       modal.hidden = !modal.hidden;
       break;
@@ -238,6 +237,7 @@ function userPicked(whichOne) {
 
 //MODALS//
 const modal = document.getElementById('modal');
+const help = document.getElementById('help');
  
 const closeModalObj = document.getElementById('btn-modal-close');
 closeModalObj.addEventListener('click', function(){
